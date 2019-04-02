a50cd6d80a5c03097977962864685e99ab5249b4
github username <github email address>
2019-04-02 14:52:50 -0400

    css change

--- src/Components/DogCard.js
+++ src/Components/DogCard.js
@@ -4,7 +4,7 @@ class DogCard extends React.Component {
   render() {
     return (
       <div>
-        <h2>{/*Dog name goes here*/}</h2>
+        <h2 className="bark">{/*Dog name goes here*/}</h2>
         <img alt="" src={""} />
         <button onClick={""}>Bark</button>
       </div>
